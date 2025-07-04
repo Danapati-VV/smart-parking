b4e539f3f7b7ce81beb51f12b374125dba5abcbf
SonOfLightY <zidanyahya95@gmail.com>
2025-07-04 09:38:03 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return view('welcome');
 });
 
 Route::get('/login', function(){
     return view('login');
+});
+
+Route::get('/profile', function(){
+    return view('profile');
+});
+
+Route::get('/home', function(){
+    return view('home');
 });
